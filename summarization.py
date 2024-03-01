5443358ec0dbf532ab4edb8f1619e5577574fd6e
AneeshP99 <114942012+AneeshP99@users.noreply.github.com>
2024-03-01 15:49:15 +0530

    Update summarization.py

--- summarization.py
+++ summarization.py
@@ -9,9 +9,7 @@ nltk.download('punkt')
 nltk.download('stopwords')
 
 def read_article(text):
-    # Split the text into sentences
     sentences = sent_tokenize(text)
-    # Split the text into words
     words = [word_tokenize(sent.lower()) for sent in sentences]
     return sentences, words
 
@@ -26,14 +24,11 @@ def sentence_similarity(sent1, sent2, stopwords=None):
  
     vector1 = [0] * len(all_words)
     vector2 = [0] * len(all_words)
- 
-    # Build the vector for the first sentence
     for w in sent1:
         if w in stopwords:
             continue
         vector1[all_words.index(w)] += 1
  
-    # Build the vector for the second sentence
     for w in sent2:
         if w in stopwords:
             continue
@@ -42,7 +37,6 @@ def sentence_similarity(sent1, sent2, stopwords=None):
     return 1 - cosine_distance(vector1, vector2)
 
 def build_similarity_matrix(sentences, stop_words):
-    # Create an empty similarity matrix
     similarity_matrix = np.zeros((len(sentences), len(sentences)))
  
     for idx1 in range(len(sentences)):
@@ -57,24 +51,18 @@ def generate_summary(text, top_n=2):
     stop_words = stopwords.words('english')
     summarize_text = []
 
-    # Step 1 - Read text and tokenize
     sentences, words = read_article(text)
 
-    # Step 2 - Generate Similarity Matrix across sentences
     sentence_similarity_matrix = build_similarity_matrix(words, stop_words)
 
-    # Step 3 - Rank sentences in similarity matrix
     sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_matrix)
     scores = nx.pagerank(sentence_similarity_graph)
 
-    # Step 4 - Sort the ranked sentences
     ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)
 
-    # Step 5 - Get the top sentences as the summary
     for i in range(top_n):
       summarize_text.append(" ".join(ranked_sentences[i][1]))
 
-    # Step 6 - Combine the sentences to generate the summary
     summary = " ".join(summarize_text)
     return summary
 
